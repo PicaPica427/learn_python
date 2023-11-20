c433f78a30f62d0a627fe2eb6b9aa0bf2ac10675
Yuki <2260330761@qq.com>
2023-11-21 00:06:18 +0800

    2023-11-21 1

---
+++ a.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding:UTF-8 -*-
+##########################################################################
+# File Name: a.py
+# Author: cx
+# Created Time: Mon Nov 20 23:18:28 2023
+##########################################################################
+
+# https://www.w3schools.com/python/python_lists.asp
+
+thislist = ["apple","banana","cherr", 1]
+print(thislist)
+print(len(thislist))
+
+
+# type(list) is list 
+print(type(thislist))
+
+for i in thislist:
+	print(i,end=' ...  ')
+
+print("--------------------")
+
+
+# type((1,2,3)) is Tuple 
+print(type((1,2)))
+
+
+
+#	There are four collection data types in the Python programming language:
+	# List is a collection which is ordered and changeable. Allows duplicate members.
+	# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+	# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+	# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
